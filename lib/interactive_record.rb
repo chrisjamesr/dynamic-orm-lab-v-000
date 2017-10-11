eecfef27c0b10dabeed739641b51724c7d34ef3a
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-11 01:36:15 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -30,7 +30,13 @@ class InteractiveRecord
       col == "id"}.join(", ")
   end
 
-  
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end  
+      
+  end
 
 
 

e3d1e556d0c6a2c2221186123e7cb035352e0ac4
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-11 01:04:21 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,7 +8,22 @@ class InteractiveRecord
     "#{self.to_s.downcase.pluralize}"
   end
 
+  def initialize
+
+  end
 
   def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = <<-SQL
+      "PRAGMA table_info('#{self.table_name}')"
+    SQL
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |column|
+      column_names << column["name"]
+    end
   end
-end
+
+
+
+end  #  End of Class

837622fa203c00f0504df196815c4534aeb856a1
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-11 02:17:13 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
     DB[:conn].execute(sql, name)
   end
 
-  def self.find_by(attribute:)
+  def self.find_by(attribute)
     sql = <<-SQL
       SELECT * FROM #{self.table_name}
       WHERE ? = ?

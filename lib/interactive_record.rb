6da82e57a0f602a6cfd0d238e7d93425fb00b4f8
Chris Rudolph <chrisjames.r@gmail.com>
2017-10-11 02:56:22 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -59,7 +59,7 @@ class InteractiveRecord
 
   def self.find_by(attribute)
     value = attribute.values.first
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = #{formatted_value}"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.keys.first} = #{value}"
     DB[:conn].execute(sql)
   end
 
